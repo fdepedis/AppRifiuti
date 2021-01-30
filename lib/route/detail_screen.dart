fc598aa1a383d9da612c7d887ef421bc937c51e5
Flavio <flavio.depedis@gmail.com>
2021-01-31 00:42:03 +0100

    add Strings

--- lib/route/detail_screen.dart
+++ lib/route/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:math' as math;
 
+import 'package:ardea_rifiuti/utils/strings.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart' show timeDilation;
@@ -44,7 +45,8 @@ class RadialExpansion extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipOval(
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15.0),
       child: Center(
         child: SizedBox(
           width: clipRectSize,
@@ -64,7 +66,7 @@ class RadialExpansion extends StatelessWidget {
 }
 
 class RadialExpansionDemo extends StatelessWidget {
-  static const double kMinRadius = 32.0;
+  static const double kMinRadius = 42.0;
   static const double kMaxRadius = 128.0;
   static const opacityCurve =
       const Interval(0.0, 0.75, curve: Curves.fastOutSlowIn);
@@ -83,10 +85,12 @@ class RadialExpansionDemo extends StatelessWidget {
       color: Theme.of(context).canvasColor,
       child: Center(
         child: Card(
+          margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
           elevation: 8.0,
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
+              /*Text(imageName),*/
               SizedBox(
                 width: kMaxRadius * 2.0,
                 height: kMaxRadius * 2.0,
@@ -104,12 +108,15 @@ class RadialExpansionDemo extends StatelessWidget {
                   ),
                 ),
               ),
-              AutoSizeText(
-                description,
-                maxLines: 100,
-                style: TextStyle(fontSize: 24),
-                minFontSize: 16,
-                overflow: TextOverflow.ellipsis,
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: AutoSizeText(
+                  description,
+                  maxLines: 100,
+                  style: TextStyle(fontSize: 24),
+                  minFontSize: 16,
+                  overflow: TextOverflow.ellipsis,
+                ),
               ),
               /*const SizedBox(height: 16.0),*/
             ],
@@ -138,13 +145,14 @@ class RadialExpansionDemo extends StatelessWidget {
                       Animation<double> animation,
                       Animation<double> secondaryAnimation) {
                     return AnimatedBuilder(
-                        animation: animation,
-                        builder: (BuildContext context, Widget child) {
-                          return Opacity(
-                            opacity: opacityCurve.transform(animation.value),
-                            child: _buildPage(context, imageName, description),
-                          );
-                        });
+                      animation: animation,
+                      builder: (BuildContext context, Widget child) {
+                        return Opacity(
+                          opacity: opacityCurve.transform(animation.value),
+                          child: _buildPage(context, imageName, description),
+                        );
+                      },
+                    );
                   },
                 ),
               );
@@ -169,14 +177,14 @@ class RadialExpansionDemo extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            _buildHero(context, 'images/AbitiDismessiR.png',
-                'Gli indumenti dismessi possono essere conferiti, tutti i giorni negli appositi contenitori di colore giallo dislocati sul territorio cittadino. Non depositare indumenti o buste nei pressi dei contenitori.'),
-            _buildHero(context, 'images/IngombrantiR.png', 'Ingombranti'),
-            _buildHero(context, 'images/OliiVegetaliR.png', 'Olii Vegetali'),
-            _buildHero(context, 'images/RaeeR.png', 'RAEE'),
-            _buildHero(context, 'images/RupR.png', 'RUP'),
-            _buildHero(context, 'images/TonerR.png', 'Toner'),
-            _buildHero(context, 'images/VetroR.png', 'Vetro'),
+            _buildHero(
+                context, 'images/AbitiDismessiR.png', Strings.abiti_dismessi),
+            _buildHero(context, 'images/IngombrantiR.png', Strings.ingombranti),
+            _buildHero(context, 'images/OliiVegetaliR.png', Strings.olii),
+            _buildHero(context, 'images/RaeeR.png', Strings.raee),
+            _buildHero(context, 'images/RupR.png', Strings.rup),
+            _buildHero(context, 'images/TonerR.png', Strings.toner),
+            _buildHero(context, 'images/VetroR.png', Strings.vetro),
           ],
         ),
       ),
