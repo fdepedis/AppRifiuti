37a1e863d687bcac20867b8fc708f793a14941ba
Flavio <flavio.depedis@gmail.com>
2021-02-01 15:01:42 +0100

    fix dimension in splash_screen

--- lib/route/splash_screen.dart
+++ lib/route/splash_screen.dart
@@ -29,33 +29,56 @@ class _SplashScreenState extends State<SplashScreen> {
       body: SafeArea(
         child: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Text(
-                'App Rifiuti',
-                style: TextStyle(
-                    fontFamily: 'Pacifico',
-                    fontSize: 40.0,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 40.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.teal.shade100,
-                  thickness: 1,
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 150.0,
+                    ),
+                    Text(
+                      'App Rifiuti',
+                      style: TextStyle(
+                          fontFamily: 'Pacifico',
+                          fontSize: 40.0,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                      width: 150.0,
+                      child: Divider(
+                        color: Colors.teal.shade100,
+                        thickness: 1,
+                      ),
+                    ),
+                    Text(
+                      'Marina Tor San Lorenzo',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20.0,
+                        color: Colors.teal[100],
+                        //color: Colors.shade100,
+                        letterSpacing: 2.5,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Text(
-                'Marina Tor San Lorenzo',
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  fontSize: 20.0,
-                  color: Colors.teal[100],
-                  //color: Colors.shade100,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  child: Text(
+                    'developed by Top1 - rev.1.0',
+                    style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 14.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
                 ),
               ),
             ],
