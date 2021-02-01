ab2440c7c70961d8501bfa5fd3482b6d295215f9
Flavio <flavio.depedis@gmail.com>
2021-02-01 11:37:29 +0100

    refactor class

---
+++ lib/content/radial_expansion_content.dart
@@ -0,0 +1,203 @@
+import 'dart:math' as math;
+
+import 'package:ardea_rifiuti/utils/strings.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+
+class Photo extends StatelessWidget {
+  Photo({Key key, this.photo, this.color, this.onTap}) : super(key: key);
+
+  final String photo;
+  final Color color;
+  final VoidCallback onTap;
+
+  Widget build(BuildContext context) {
+    return Material(
+      // Slightly opaque color appears where the image has transparency.
+      color: Theme.of(context).primaryColor.withOpacity(0.25),
+      child: InkWell(
+        onTap: onTap,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints size) {
+            return Image.asset(
+              photo,
+              fit: BoxFit.contain,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class RadialExpansion extends StatelessWidget {
+  RadialExpansion({
+    Key key,
+    this.maxRadius,
+    this.child,
+  })  : clipRectSize = 2.0 * (maxRadius / math.sqrt2),
+        super(key: key);
+
+  final double maxRadius;
+  final clipRectSize;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(15.0),
+      child: Center(
+        child: SizedBox(
+          width: clipRectSize,
+          height: clipRectSize,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15.0),
+            child: Container(
+              height: 83.0,
+              width: 83.0,
+              child: child,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RadialExpansionContent extends StatelessWidget {
+  static const double kMinRadius = 42.0;
+  static const double kMaxRadius = 128.0;
+  static const opacityCurve =
+      const Interval(0.0, 0.75, curve: Curves.fastOutSlowIn);
+
+  final String titleCard;
+
+  RadialExpansionContent({Key key, @required this.titleCard}) : super(key: key);
+
+  static RectTween _createRectTween(Rect begin, Rect end) {
+    return MaterialRectCenterArcTween(begin: begin, end: end);
+  }
+
+  static Widget _buildPage(
+      BuildContext context, String imageName, String description) {
+    return Container(
+      color: Theme.of(context).canvasColor,
+      child: Center(
+        child: Card(
+          margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+          elevation: 8.0,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              /*Text(imageName),*/
+              SizedBox(
+                width: kMaxRadius * 2.0,
+                height: kMaxRadius * 2.0,
+                child: Hero(
+                  createRectTween: _createRectTween,
+                  tag: imageName,
+                  child: RadialExpansion(
+                    maxRadius: kMaxRadius,
+                    child: Photo(
+                      photo: imageName,
+                      onTap: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: AutoSizeText(
+                  description,
+                  maxLines: 100,
+                  style: TextStyle(fontSize: 24),
+                  minFontSize: 16,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ),
+              /*const SizedBox(height: 16.0),*/
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHero(
+      BuildContext context, String imageName, String description) {
+    return Container(
+      width: kMinRadius * 2.0,
+      height: kMinRadius * 2.0,
+      child: Hero(
+        createRectTween: _createRectTween,
+        tag: imageName,
+        child: RadialExpansion(
+          maxRadius: kMaxRadius,
+          child: Photo(
+            photo: imageName,
+            onTap: () {
+              Navigator.of(context).push(
+                PageRouteBuilder<void>(
+                  pageBuilder: (BuildContext context,
+                      Animation<double> animation,
+                      Animation<double> secondaryAnimation) {
+                    return AnimatedBuilder(
+                      animation: animation,
+                      builder: (BuildContext context, Widget child) {
+                        return Opacity(
+                          opacity: opacityCurve.transform(animation.value),
+                          child: _buildPage(context, imageName, description),
+                        );
+                      },
+                    );
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    timeDilation = 2.5; // 1.0 is normal animation speed.
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.teal,
+        title: Text(
+          titleCard,
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Pacifico',
+            fontSize: 20,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(22.0),
+        alignment: FractionalOffset.bottomLeft,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _buildHero(
+                context, 'images/AbitiDismessiR.png', Strings.abiti_dismessi),
+            _buildHero(context, 'images/IngombrantiR.png', Strings.ingombranti),
+            _buildHero(context, 'images/OliiVegetaliR.png', Strings.olii),
+            _buildHero(context, 'images/RaeeR.png', Strings.raee),
+            _buildHero(context, 'images/RupR.png', Strings.rup),
+            _buildHero(context, 'images/TonerR.png', Strings.toner),
+            _buildHero(context, 'images/VetroR.png', Strings.vetro),
+          ],
+        ),
+      ),
+    );
+  }
+}
